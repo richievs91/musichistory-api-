8f141eedf93c2af3dd76530902c2f5be56872eb4
Richard Van Sickle <rvansickle91@gmail.com>
2017-02-03 12:50:43 -0600

    changed names of attributes with id at the end

--- musichistory_api/music_api/models.py
+++ musichistory_api/music_api/models.py
@@ -20,8 +20,8 @@ class Album(models.Model):
     release_date = models.CharField(max_length=55)
     album_length = models.CharField(max_length=55)
     label = models.CharField(max_length=55)
-    artist_id = models.ForeignKey(Artist, blank=True, default='')
-    genre_id = models.ForeignKey(Genre, blank=True, default='')
+    artist = models.ForeignKey(Artist, blank=True, default='')
+    genre = models.ForeignKey(Genre, blank=True, default='')
 
     def __str__(self):
         return '{} {} {} {}'.format(self.album_title, self.release_date, self.album_length, self.label)
@@ -30,9 +30,9 @@ class Song(models.Model):
     song_title = models.CharField(max_length=55)
     song_length = models.CharField(max_length=55)
     release_date = models.CharField(max_length=55)
-    genre_id = models.ForeignKey(Genre, blank=True, default='')
-    artist_id = models.ForeignKey(Artist, blank=True, default='')
-    album_id = models.ForeignKey(Album, blank=True, default='')
+    genre = models.ForeignKey(Genre, blank=True, default='')
+    artist = models.ForeignKey(Artist, blank=True, default='')
+    album = models.ForeignKey(Album, blank=True, default='')
 
     def __str__(self):
         return '{} {} {}'.format(self.song_title, self.song_length, self.release_date)
